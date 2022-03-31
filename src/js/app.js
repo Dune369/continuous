7bc2414629f176e370dbc4e7f0f252fa8b57758d
Dinar <sagitoff.dinar@yandex.ru>
2022-03-31 20:39:41 +0300

    на проверку

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
-// TODO: write code here
-
-// comment this to pass build
-// const unusedVariable = 'variable';
-
-// for demonstration purpose only
-export default function demo(value) {
-  return value;
+export default function displayTheLifeBar(object) {
+  const healthy = 'healthy';
+  const wounded = 'wounded';
+  const critical = 'critical';
+  if (object.health >= 50) {
+    return healthy;
+  } if (object.health <= 50 && object.health >= 15) {
+    return wounded;
+  }
+  return critical;
 }
